8d97b2dd331c10f1e9cc07221ec0b19ffa1b2b70
a885566885566 <a885566885566@gmail.com>
2020-07-17 14:13:20 +0800

    Add motion planner

--- NavigationSystem/src/MotionPlanner.py
+++ NavigationSystem/src/MotionPlanner.py
@@ -1,3 +1,6 @@
+import numpy as np
+import WaypointsPlanner as wp 
+import Perception
 """
 This class create some potential path, called TargetSet
 to imitate future motion, and determine the best path 
@@ -15,10 +18,28 @@ class MotionPlanner():
 
     """
     Create potential path.
+    Input: 
+        current_state: 4d nparray, in global coordinate 
+        goal_waypoint: 4d nparray, in global coordinate
+        num_of_points: odd number, number of point in target_set
+        offset: number, distance between `target_set` points 
     Output:
         path: 2d Array in local coordinate.
     """
-    def generate_path(self):
+    def generate_path(self, current_state, goal_waypoint, num_of_points, offset):
+        vec_goal = goal_waypoint - current_state 
+        vec_del = np.array([vec_goal[0]+offset*np.cos(vec_goal[2]+np.pi/2),
+                            vec_goal[1]+offset*np.sin(vec_goal[2]+np.pi/2),
+                            0,
+                            goal_waypoint[3]])
+        self.target_set = np.zeros(num_of_points, 4)
+        mid_idx = num_of_points // 2
+        for i in range(mid_idx):
+            if i == 0:
+                self.target_set[mid_idx] = vec_goal
+            else:
+                self.target_set[mid_idx+i] = vec_goal + 
+                
         return
 
     """
