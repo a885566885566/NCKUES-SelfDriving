f08cff699de0d8c209fd2dc93bbbdb7ad41df4af
as0027111 <as0027111@gmail.com>
2020-07-17 16:34:43 +0800

    complete the make_score function in MotionPlanner.py and add a little test main function code.

--- NavigationSystem/src/MotionPlanner.py
+++ NavigationSystem/src/MotionPlanner.py
@@ -11,8 +11,7 @@ class MotionPlanner():
     This class require `WaypointPlanner` object to obtain
     waypoint information.
     """
-    def __init__(self, waypoints):
-        self.waypoints = waypoints
+    def __init__(self):
         self.target_set = None
         return
 
@@ -44,14 +43,14 @@ class MotionPlanner():
             if i == 0:
                 self.target_set[mid_idx] = vec_goal
             else:
-                self.target_set[mid_idx+i] = vec_goal + 
+                self.target_set[mid_idx+i] = vec_goal
                 
         return
 
     """
     Get the obstacle information and check if the path's validity.
     Input:
-        paths, obstacles
+        path_set, obstacles
     Ouput:
         path_validity: 1d array of boolen value to classify whether a path is collision-free(True), or not(False). 
     """
@@ -62,25 +61,52 @@ class MotionPlanner():
     Make score to a specific path.
     Input:
         path_set: A list of path in the local coordinate.
+        
         path: A 2d nparray in local coordinate, Content is a list of points.
             format: [[x_points, y_point, t, velocity]
                      [                              ]
                      ................................]
+        
         goal_state: current goal waypoint for the vehicle to reach in local coordinate.
             format: [x_goal, y_goal, t, v_goal]
         
+        path_validity: 1d array of boolen value to classify whether a path is collision-free(True), or not(False). 
     Output:
-        score: A 1d array to indicate the score of the specific path. 
+        score: A 1d array to indicate the score of the specific path,
+               a low score implies the suitable path, 
+               -1 represents the colliding path's score.
     """
-    def make_score(self, path_set, current_goal_waypoint):
+    def make_score(self, path_set, goal_state, path_validity):
         
-        return
+        score = []
+
+        for i in range(len(path_set)):
+            # Handle the case of collision-free path.
+
+            if path_validity[i]:
+                # Compute the collision-free path goal point's distance to centerline goal point.
+                # A low score implies the suitable path.
+
+                score.append( np.sqrt( (path_set[i][-1][0] - goal_state[0])**2 + (path_set[i][-1][1] - goal_state[1])**2 ) )
+
+                for j in range(len(path_set)):
+                    # Compute the collision-free path goal point's distance to colliding path's goal point
+                    # then substract it from the score, farther distance to the colliding path implies the suitable path. 
+                    if j == i:
+                        continue
+                    if not path_validity[j]:
+                        score[i] -= np.sqrt( (path_set[i][-1][0] - path_set[j][-1][0])**2 + (path_set[i][-1][1] - path_set[j][-1][1])**2 )
+
+            else:
+                score.append(-1) 
+        return score
 
     """
     Choose best path according to `make_score` function.
     Output: path, 2d Array
     """
     def choose_best_path(self):
+    # TODO: need to find a way to choose a path among the same score path
         return
 
     def generate_emergency_path(self):
@@ -88,4 +114,15 @@ class MotionPlanner():
 
     def generate_velocity_profile(self):
         return 
+
+if __name__ == "__main__":
+
+    mp = MotionPlanner()
+    path_set = [ [[20,60]], [[20,55]], [[20,50]], [[20,45]], [[20,40]] ]
+    goal_state = [20,50]
+    path_validity = [True, False, True, True, True]
+    score = mp.make_score(path_set, goal_state, path_validity)
+
+    for i in range(len(score)):
+        print( "the", (i+1), "path's score is", score[i])
 
