f15cf10a6375169cb462aa986b6d0d1778b173e5
a885566885566 <a885566885566@gmail.com>
2020-09-11 11:25:53 +0800

    Rewrite to match new can communication protocal

--- NavigationSystem/src/CarCommunication.py
+++ NavigationSystem/src/CarCommunication.py
@@ -1,38 +1,60 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import can	# Import python-can module
+# Import python-can module
+import can	
+import numpy as np
 import time
 import struct
 
-NAVIGATOR_OFFSETED_Id= 0x30
+from enum import Enum
+
+NAVIGATOR_OFFSETED_ID = 0x30
+STM_BUFFER_LEN = 5
+
+class Terminator(Enum):
+    RESET = 0
+    ORIGIN = 1
+    UPDATE = 2
+    PATH = 3
 
 class CanHandle(can.Listener):
     def __init__(self):
-        self.send_period = 1
-        self.num_msg = 4
-
+        # Total number of message including terminate message
+        self.num_msg = STM_BUFFER_LEN
+        
         self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
-        self.recv_msg = can.Message(arbitration_id=NAVIGATOR_OFFSETED_Id, data=[0,0,0,0,0,0,0,0], is_extended_id=False)
+
+        # msg buffer for receiving
+        self.recv_msg = can.Message(arbitration_id=0, 
+                data=[0,0,0,0], is_extended_id=False)
+
+        # msg buffer for tranciving
         self.tran_msg = []
         for i in range(self.num_msg):
-            self.tran_msg.append(can.Message(arbitration_id=0x30 + i, data=[0,0,0,0,0,0,0,0], is_extended_id=False))
+            self.tran_msg.append(can.Message(
+                arbitration_id=NAVIGATOR_OFFSETED_ID + i, 
+                data=[0,0,0,0], is_extended_id=False))
 
-    def start(self):
-        for i in range(self.num_msg):
-            #self.bus.send_periodic(self.tran_msg[i], self.send_period)
-            pass
-
+    """
+    Send msg to navigator, the message format require the following format
+    msg={
+        mode= `Terminator`
+        data= 1d numpy_array, the length must be smaller than CAN_BUFFER_LEN
+    }
+    """
     def send_msg(self, msg):
-        for i in range(self.num_msg):
-            self.tran_msg[i].data = bytearray(struct.pack("f", msg[i]))
+        for i in range(min(self.num_msg-1, msg['data'].shape[0])):
+            self.tran_msg[i].data = bytearray(struct.pack("f", msg['data'][i]))
             self.bus.send(self.tran_msg[i])
-            
+        # Terminate message
+        self.tran_msg[self.num_msg-1].data = bytearray(struct.pack("I", msg['mode'].value))
+        self.bus.send(self.tran_msg[self.num_msg-1])
 
-    def close(self):
-        """
-        Close all bus task
-        """
+    """
+    Close all bus task and shutdown can bus
+    """
+    def close(self):    
         self.bus.stop_all_periodic_tasks()
         self.bus.shutdown()
 
@@ -43,16 +65,23 @@ class CanHandle(can.Listener):
 
 def main():
     can = CanHandle()
-    msg = [6.3, 5.2, 3.7, 4.4]
-    can.send_msg(msg)
-    can.start()
+    msg = {}
+    msg['mode'] = Terminator.PATH
+    msg['data'] = np.array([6.3, 5.2, 3.7, 4.4])
+
+    ori = {}
+    ori['mode'] = Terminator.ORIGIN
+    ori['data'] = np.array([6.3, 5.2, 3.7, 4.4])
     for i in range(20):
-        print("Set")
         for j in range(4):
-            msg[j] = j + 0.1*i
+            msg['data'][j] = j + 0.1*i
+            ori['data'][j] = 0.1*j + i
         can.send_msg(msg)
+        can.send_msg(ori)
         print(msg)
+        print(ori)
         time.sleep(0.5)
+    time.sleep(10)
     can.close()
 
 if __name__ == "__main__":
